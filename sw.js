6aec36ad793ddeeea41643cdf78b0248ea33fca4
resoner0796 <ulisees.luna96@gmail.com>
2025-09-17 20:56:50 -0500

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@ const urlsToCache = [
   'index.html',
   'manifest.json',
   // Asegúrate que las rutas a tus iconos sean correctas
-  'images/icons/icon-192.png',
-  'images/icons/icon-512.png'
+  'icon-192x192.PNG',
+  'icon-512x512.PNG'
 ];
 
 // Evento 'install': se dispara cuando el Service Worker se instala.
